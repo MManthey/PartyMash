9a061e40de4f0e3202acf7181dc54220a9473ac8
Michael Smirnov <michael.smirnov@gmx.de>
2020-12-29 14:27:32 +0100

    CreateRoundTournament mit Optionen befuellt

--- src/pages/CreateRoundTournament.js
+++ src/pages/CreateRoundTournament.js
@@ -1,7 +1,21 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+import Divider from "@material-ui/core/Divider";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    height: 60,
+  },
+}));
 
 function CreateRoundTournament() {
+  const classes = useStyles();
+
   return (
     <div>
       <NavBar
@@ -9,7 +23,51 @@ function CreateRoundTournament() {
         text="Rundenturnier Erstellen"
         rightSide="info"
       />
-      CreateTournament Einstellungen
+      <Grid
+        style={{ marginTop: 16, marginBottom: 16 }}
+        container
+        direction="column"
+        justify="space-evenly"
+        alignItems="stretch"
+        spacing={6}
+      >
+        <Grid item>
+          <Typography>Turnierrunden</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Teams erlauben</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Teamgröße beschränken</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Max. Teamgröße</Typography>
+        </Grid>
+        <Divider variant="middle" />
+        <Grid item>
+          <Typography>Punkte für Sieg</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Punkte für Unentschieden</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Punkte für Niederlage</Typography>
+        </Grid>
+        <Divider variant="middle" />
+        <Grid item>
+          <Typography>Anzahl der Parallelen Spiele</Typography>
+        </Grid>
+      </Grid>
+      <Button
+        className={classes.button}
+        color="secondary"
+        variant="contained"
+        size="large"
+        component={Link}
+        to="/teamZuordnen"
+      >
+        <Typography variant="h6">Lobby erstellen</Typography>
+      </Button>
     </div>
   );
 }
