3d76633f963105d810fe393e9732d1fa6a6323f5
Michael Smirnov <michael.smirnov@gmx.de>
2020-12-30 14:54:39 +0100

    Syling der Erstellseiten gefixed

--- src/pages/CreateKnockoutTournament.js
+++ src/pages/CreateKnockoutTournament.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import SettingsElement from "../components/SettingsElement";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -18,17 +19,23 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: 32,
   },
   grid: {
-    marginTop: 16,
-    marginBottom: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
+    // marginTop: 16,
+    // marginBottom: 16,
+    // paddingLeft: 16,
+    // paddingRight: 16,
   },
 }));
 
 function renderSettings(items) {
   return items.map((item) => (
     <Grid item>
-      <SettingsElement id={item.id} text={item.text} controlElement={item.controlElement} maxValue={item.maxValue} minValue={item.minValue} />
+      <SettingsElement
+        id={item.id}
+        text={item.text}
+        controlElement={item.controlElement}
+        maxValue={item.maxValue}
+        minValue={item.minValue}
+      />
     </Grid>
   ));
 }
@@ -39,16 +46,27 @@ function CreateRoundTournament() {
   const settingElementsTop = [
     { id: 1, text: "Teams erlauben", controlElement: "switch" },
     { id: 2, text: "Teamgröße beschränken", controlElement: "switch" },
-    { id: 3, text: "Max. Teamgröße", controlElement: "incrementer", maxValue: 10, minValue: 0 },
+    {
+      id: 3,
+      text: "Max. Teamgröße",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
   ];
 
   const settingElementsMiddle = [
     { id: 10, text: "Double Elemination", controlElement: "switch" },
-
   ];
 
   const settingElementsBottom = [
-    { id: 103, text: "Anzahl der Parallelen Spiele", controlElement: "incrementer", maxValue: 10, minValue: 0 },
+    {
+      id: 103,
+      text: "Anzahl der Parallelen Spiele",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
   ];
 
   return (
@@ -58,34 +76,45 @@ function CreateRoundTournament() {
         text="Rundenturnier Erstellen"
         rightSide="info"
       />
-      <Grid
-        className={classes.grid}
-        container
-        direction="column"
-        justify="space-around"
-        alignItems="stretch"
-        spacing={6}
-      >
-        {renderSettings(settingElementsTop)}
-        <Divider variant="middle" />
-        {renderSettings(settingElementsMiddle)}
-        <Divider variant="middle" />
-        {renderSettings(settingElementsBottom)}
-      </Grid>
-      <Grid container justify="center">
-        <Grid item>
-          <Button
-            className={classes.button}
-            color="secondary"
-            variant="contained"
-            size="large"
-            component={Link}
-            to="/teamZuordnen"
-          >
-            <Typography variant="h6">Lobby erstellen</Typography>
-          </Button>
+      {/**Spacing lieber mit den Material UI eingabuten sachen  https://material-ui.com/system/spacing/*/}
+      <Box mx={2} my={2}>
+        {/**https://stackoverflow.com/questions/45519275/grid-in-material-ui-causes-horizontal-scroll-react*/}
+        <Grid
+          // className={classes.grid}
+          container
+          direction="column"
+          justify="space-around"
+          alignItems="stretch"
+          // {/** spacing führt dazu dass du einen Rand rechts bekommst den du scrollen kannst */}
+          spacing={4}
+          style={{
+            margin: 0,
+            width: "100%",
+          }}
+        >
+          {renderSettings(settingElementsTop)}
+          <Divider variant="middle" />
+          {renderSettings(settingElementsMiddle)}
+          <Divider variant="middle" />
+          {renderSettings(settingElementsBottom)}
         </Grid>
-      </Grid>
+        <Grid container justify="center">
+          <Grid item>
+            <Box mt={4}>
+              <Button
+                className={classes.button}
+                color="secondary"
+                variant="contained"
+                size="large"
+                component={Link}
+                to="/teamZuordnen"
+              >
+                <Typography variant="h6">Lobby erstellen</Typography>
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
--- src/pages/CreateRoundTournament.js
+++ src/pages/CreateRoundTournament.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import SettingsElement from "../components/SettingsElement";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -18,17 +19,23 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: 32,
   },
   grid: {
-    marginTop: 16,
-    marginBottom: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
+    // marginTop: 16,
+    // marginBottom: 16,
+    // paddingLeft: 16,
+    // paddingRight: 16,
   },
 }));
 
 function renderSettings(items) {
   return items.map((item) => (
     <Grid item>
-      <SettingsElement id={item.id} text={item.text} controlElement={item.controlElement} maxValue={item.maxValue} minValue={item.minValue} />
+      <SettingsElement
+        id={item.id}
+        text={item.text}
+        controlElement={item.controlElement}
+        maxValue={item.maxValue}
+        minValue={item.minValue}
+      />
     </Grid>
   ));
 }
@@ -37,20 +44,56 @@ function CreateRoundTournament() {
   const classes = useStyles();
 
   const settingElementsTop = [
-    { id: 1, text: "Anzahl Turnierrunden", controlElement: "incrementer", maxValue: 10, minValue: 0 },
+    {
+      id: 1,
+      text: "Anzahl Turnierrunden",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
     { id: 2, text: "Teams erlauben", controlElement: "switch" },
     { id: 3, text: "Teamgröße beschränken", controlElement: "switch" },
-    { id: 4, text: "Max. Teamgröße", controlElement: "incrementer", maxValue: 10, minValue: 0 },
+    {
+      id: 4,
+      text: "Max. Teamgröße",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
   ];
 
   const settingElementsMiddle = [
-    { id: 100, text: "Punkte für Sieg", controlElement: "incrementer", maxValue: 10, minValue: 0 },
-    { id: 101, text: "Punkte für Unentschieden", controlElement: "incrementer", maxValue: 10, minValue: 0 },
-    { id: 102, text: "Punkte für Niederlage", controlElement: "incrementer", maxValue: 10, minValue: 0 },
+    {
+      id: 100,
+      text: "Punkte für Sieg",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
+    {
+      id: 101,
+      text: "Punkte für Unentschieden",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
+    {
+      id: 102,
+      text: "Punkte für Niederlage",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
   ];
 
   const settingElementsBottom = [
-    { id: 103, text: "Anzahl der Parallelen Spiele", controlElement: "incrementer", maxValue: 10, minValue: 0 },
+    {
+      id: 103,
+      text: "Anzahl der Parallelen Spiele",
+      controlElement: "incrementer",
+      maxValue: 10,
+      minValue: 0,
+    },
   ];
 
   return (
@@ -60,34 +103,44 @@ function CreateRoundTournament() {
         text="Rundenturnier Erstellen"
         rightSide="info"
       />
-      <Grid
-        className={classes.grid}
-        container
-        direction="column"
-        justify="space-evenly"
-        alignItems="stretch"
-        spacing={6}
-      >
-        {renderSettings(settingElementsTop)}
-        <Divider variant="middle" />
-        {renderSettings(settingElementsMiddle)}
-        <Divider variant="middle" />
-        {renderSettings(settingElementsBottom)}
-      </Grid>
-      <Grid container justify="center">
-        <Grid item>
-          <Button
-            className={classes.button}
-            color="secondary"
-            variant="contained"
-            size="large"
-            component={Link}
-            to="/teamZuordnen"
-          >
-            <Typography variant="h6">Lobby erstellen</Typography>
-          </Button>
+      {/**Spacing lieber mit den Material UI eingabuten sachen  https://material-ui.com/system/spacing/*/}
+      <Box mx={2} my={2}>
+        {/**https://stackoverflow.com/questions/45519275/grid-in-material-ui-causes-horizontal-scroll-react*/}
+        <Grid
+          // className={classes.grid}
+          container
+          direction="column"
+          justify="space-evenly"
+          alignItems="stretch"
+          spacing={4}
+          style={{
+            margin: 0,
+            width: "100%",
+          }}
+        >
+          {renderSettings(settingElementsTop)}
+          <Divider variant="middle" />
+          {renderSettings(settingElementsMiddle)}
+          <Divider variant="middle" />
+          {renderSettings(settingElementsBottom)}
         </Grid>
-      </Grid>
+        <Grid container justify="center">
+          <Grid item>
+            <Box mt={4}>
+              <Button
+                className={classes.button}
+                color="secondary"
+                variant="contained"
+                size="large"
+                component={Link}
+                to="/teamZuordnen"
+              >
+                <Typography variant="h6">Lobby erstellen</Typography>
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
