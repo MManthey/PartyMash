3fdcd658cc087bd98bb45f87008dad333ac3f88b
Michael Smirnov <michael.smirnov@gmx.de>
2021-01-05 11:06:48 +0100

    QROverlay Image angepasst

--- src/components/QROverlay.js
+++ src/components/QROverlay.js
@@ -1,67 +1,68 @@
 import React from "react";
-import { Button} from "@material-ui/core";
-//import { useHistory } from "react-router-dom";
+import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import ShareIcon from '@material-ui/icons/Share'
+import ShareIcon from "@material-ui/icons/Share";
+import Box from "@material-ui/core/Box";
+import Image from "material-ui-image";
 
 function QROverlay(props) {
-
-    const useStyles = makeStyles((theme) => ({
-        button: {
-          margin: "0 auto"
-        },
-      }));
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 345,
+    },
+    button: {
+      margin: "0 auto",
+    },
+    media: {
+      height: 150,
+      width: 150,
+    },
+  }));
 
   const dialogStyle = {
-    textAlign: "center"
-  }
-
-  const imageStyle = {
-    paddingLeft: "35%",
-    paddingRight: "35%",
-    paddingTop: "5%",
-    paddingBottom: "10%"
-  }
+    textAlign: "center",
+  };
 
   const buttonStyle = {
-    marginBottom : "10%"
-  }
+    marginBottom: "10%",
+  };
 
   const classes = useStyles();
 
   return (
     <Dialog
-        aria-labelledby="simple-dialog-title"
-        open={props.open}
-        onClose={props.close}
-        style={dialogStyle}>
+      aria-labelledby="simple-dialog-title"
+      open={props.open}
+      onClose={props.close}
+      style={dialogStyle}
+    >
+      <Typography style={{ paddingTop: "15%" }} variant="h1">
+        Lobby erstellt!
+      </Typography>
+      <p style={{ padding: "10px" }}>Teile den Code mit allen Teilnehmern</p>
+      <p>Turniercode:</p>
+      <Typography variant="h1">45583</Typography>
+      <Box mx={5} my={5}>
+        <Image src="qr.png" />
+      </Box>
 
-            <Typography style={{paddingTop: "15%"}} variant="h1">Lobby erstellt!</Typography>
-            <p style={{padding: "10px"}}>Teile den Code mit allen Teilnehmern</p>
-            <p>Turniercode:</p>
-            <Typography variant="h1">45583</Typography>
-            <img style={imageStyle} src="qr.png"></img>
-
-            <Button
-              className={classes.button}
-              color="primary"
-              variant="contained"
-              size="medium"
-              component={Link}
-              to="/tournamentOverview"
-              style={buttonStyle}
-            >
-              
-              <Typography noWrap={false} variant="button">
-                Teilen
-              </Typography>
-              <ShareIcon style={{marginLeft: "10%"}}></ShareIcon>
-            </Button>
-
+      <Button
+        className={classes.button}
+        color="primary"
+        variant="contained"
+        size="medium"
+        component={Link}
+        to="/tournamentOverview"
+        style={buttonStyle}
+      >
+        <Typography noWrap={false} variant="button">
+          Teilen
+        </Typography>
+        <ShareIcon style={{ marginLeft: "10%" }}></ShareIcon>
+      </Button>
     </Dialog>
   );
 }
